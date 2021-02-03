fbafb35ede9a5e5f210bc86e9ed07aa0d57ab802
Edison Li <edison4354@gmail.com>
2021-02-03 11:09:12 -0800

    Added methods to Song class

--- Playlist.py
+++ Playlist.py
@@ -8,14 +8,16 @@ class Playlist:
   # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
 
   def add_song(self, title):
-    pass
+    return self.__first_song += Song(title)
 
 
 
   # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
 
   def find_song(self, title):
-    pass
+    for i in self.__first_song:
+      if i == title:
+        return self.__first_song.index(i)
 
 
   # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
@@ -28,7 +30,7 @@ class Playlist:
   # TODO: Create a method called length, which returns the number of songs in the playlist.
 
   def length(self):
-    pass
+    len(self.__first_song)
 
 
   # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
@@ -39,6 +41,7 @@ class Playlist:
   # 3. Song Title 3
 
   def print_songs(self):
-    pass
+    for i in self.__first_song:
+      print (f"{self.__first_song.index(i)}. {i}")
 
   
